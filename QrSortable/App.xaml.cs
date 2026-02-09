deb361c632b934b09a20fa9447f9c5011e31f276
Praveen Kumar Das <pdas@mpr-bayern.de>
2026-02-09 09:19:51 +0100

    #first commit

---
+++ QrSortable/App.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace QrSortable
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            return new Window(new AppShell());
+        }
+    }
+}
---
+++ QrSortable/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace QrSortable
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
